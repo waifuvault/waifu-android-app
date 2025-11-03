116dace1b664ea719249ad8af353c79705b0e788
VictoriqueMoe <loli@victorique.moe>
2025-11-03 15:01:20 +0000

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "moe.waifuvault.mobile"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
